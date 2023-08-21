5d0aa643edbc08267a5c0ac8ec4e60529c3b46a2
Ana Victoria <anavictoriagomes1810@gmail.com>
2023-08-21 19:32:24 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import background from "./assets/images/illustration-sign-up-mobile.svg";
+import list from "./assets/images/icon-list.svg";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <img src={background} alt="" />
+        <div className="about">
+          <h1 className="title">Stay Updated!</h1>
+          <p className="description">
+            Join 60,000+ product managers receiving monthly updates on:
+          </p>
+          <ul className="list">
+            <li className="list-item">
+              <img src={list} alt="" />
+              <p className="list-info">
+                Product discovery and building what matters
+              </p>
+            </li>
+            <li className="list-item">
+              <img src={list} alt="" />
+              <p className="list-info">
+                Measuring to ensure updates are a success
+              </p>
+            </li>
+            <li className="list-item">
+              <img src={list} alt="" />
+              <p className="list-info">And much more!</p>
+            </li>
+          </ul>
+          <form>
+            <label className="email-title">Email address</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="email@company.com"
+              required
+            ></input>
+            <a className="button">Subscribe to monthly newsletter</a>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
 
